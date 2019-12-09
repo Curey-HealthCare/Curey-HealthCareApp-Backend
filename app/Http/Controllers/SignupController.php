926bce093d7c3f706f425384db708788df686ed7
Ahmed AlMoselhy <ahmedalmoselhy.slm@gmail.com>
2019-12-09 15:38:48 +0200

    managed conflicts with existing db records before sign up

--- app/Http/Controllers/SignupController.php
+++ app/Http/Controllers/SignupController.php
@@ -35,7 +35,58 @@ class SignupController extends Controller
             'errors' => null];
 
         return response()->json($response, 200);
-    } 
+    }
+
+    public function signUp(Request $request){
+        $role = $request['user_role'];
+        $email = $request['email'];
+        $username = $request['username'];
+        $phone = $request['phone'];
+
+        $isFailed = false;
+        $data = [];
+        $errors =  [];
+
+//        Check existing records in database for conflicts
+        $all_users = user::all();
+
+        foreach ($all_users as $user){
+            if ($user['email'] = $email and $user['role_id'] = $role){
+//                Add error that this email is already in database with same user role
+                array_push($errors, 'This email is already signed up with the same user type');
+                $isFailed = true;
+            }
+            if ($user['username'] = $username){
+//                Add error that this username is used
+                array_push($errors, 'This username is already used');
+                $isFailed = true;
+            }
+            if ($user['phone'] = $phone){
+//                Add error that this phone number is used
+                array_push($errors, 'This phone number is already used');
+                $isFailed = true;
+            }
+        }
+
+        if($isFailed){
+            if($role = '1'){
+//             sign up as customer
+            }
+            elseif ($role = '2'){
+//             sign up as pharmacy, need to make the user first then get the user id for pharmacies table
+            }
+            elseif ($role = '3'){
+//             sign up as doctor, need to make the user first then get the user id for doctors table
+            }
+        }
+
+        $response = [
+            'isFailed' => $isFailed,
+            'data' => $data,
+            'errors' => $errors,
+        ];
+        return response()->json($response);
+    }
 
 }
 
