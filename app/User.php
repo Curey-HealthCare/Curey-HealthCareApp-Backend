f4b6df9e9eee6b5b242bbe46d0d5f8982288396f
Ahmed AlMoselhy <ahmedalmoselhy.slm@gmail.com>
2019-12-09 14:36:20 +0200

    hide attributes in user model from arrays

--- app/User.php
+++ app/User.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class user extends Model
 {
     public $table = "users";
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'remember_token', 'first_login', 'password'
+    ];
 }
