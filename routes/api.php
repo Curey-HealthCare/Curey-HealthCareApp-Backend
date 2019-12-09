a8965219876e12ce5f3bd1bab3777f45fd8dad73
Ahmed AlMoselhy <ahmedalmoselhy.slm@gmail.com>
2019-12-09 15:04:33 +0200

    managed conflicts before signup & saving into db

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// Sign up routes
 Route::get('signup', 'SignupController@show');
+Route::post('signup', 'signupController@signUp');
