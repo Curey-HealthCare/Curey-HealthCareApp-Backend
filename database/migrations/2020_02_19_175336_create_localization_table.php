06ec1cb16b45f911bbe6f468540e5ac78e7f0d26
ahmedalmoselhy <ahmedalmoselhy.slm@gmail.com>
2020-02-19 19:56:41 +0200

    create localization table migration

---
+++ database/migrations/2020_02_19_175336_create_localization_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLocalizationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('localization', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('key');
+            $table->string('en')
+                ->nullable(true);
+            $table->string('ar')
+                ->nullable(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('localization');
+    }
+}
