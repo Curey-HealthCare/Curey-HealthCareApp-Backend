c2514185e0bd4863a27793624a8240a58d234e5b
Ahmed M. Raslan <ahm3draslan@gmail.com>
2020-02-27 17:16:59 +0200

    Updated database seeder with all the individual seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(LocalizationSeeder::class);
+        $this->call(UserSeeder::class);
+        $this->call(DoctorSeeder::class);
+        $this->call(PharmacySeeder::class);
     }
 }
