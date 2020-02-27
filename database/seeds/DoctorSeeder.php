012ec2cedac3a1616d70016b29be61b81f72a0fc
Ahmed M. Raslan <ahm3draslan@gmail.com>
2020-02-27 17:15:48 +0200

    Added doctor seeder

---
+++ database/seeds/DoctorSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Doctor;
+use App\User;
+
+class DoctorSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+
+    public function run()
+    {
+        // First, check the localization table for content
+        if (DB::table('doctors')->get()->count() != 0) {
+
+            // Reset the id counting and clears the table
+            // Remove and re add the foreign key checks to clear the table id incerment
+            DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+            DB::table('doctors')->truncate();
+            DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+        }
+
+        // Check for doctors in users
+        $doctors = User::where('role_id', '3')->get();
+
+        // Then passes the IDs of them to the create factory, the rest is added through it
+        foreach ($doctors as $doctor){
+            factory(Doctor::class)->create([
+                'user_id' => $doctor -> id,
+            ]);
+        }
+        
+
+        $this->command->info('    ---------------------------------------');
+        $this->command->info('        Doctor table updated ¯\_(ツ)_/¯');
+        $this->command->info('    ---------------------------------------');
+    }
+}
