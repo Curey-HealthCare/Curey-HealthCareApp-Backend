e0b1b2f6a99e1470a526c8dd676f628b9eaf17fa
Ahmed M. Raslan <ahm3draslan@gmail.com>
2020-02-27 20:29:25 +0200

    Setup the new faker service provider

---
+++ app/Providers/FakerServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Providers;
+
+use Bezhanov\Faker\ProviderCollectionHelper;
+use Faker\Factory;
+use Faker\Generator;
+use Illuminate\Support\ServiceProvider;
+
+class FakerServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        $this->app->singleton(Generator::class, function () {
+            $faker = Factory::create();
+            ProviderCollectionHelper::addAllProvidersTo($faker);
+
+            return $faker;
+        });
+    }
+}
