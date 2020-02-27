76e1bba88082f7495a91d009f9531c776f52170e
Ahmed M. Raslan <ahm3draslan@gmail.com>
2020-02-27 17:15:16 +0200

    Added user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,23 +5,27 @@ use App\User;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 
-/*
-|--------------------------------------------------------------------------
-| Model Factories
-|--------------------------------------------------------------------------
-|
-| This directory should contain each of the model factory definitions for
-| your application. Factories provide a convenient way to generate new
-| model instances for testing / seeding your application's database.
-|
-*/
-
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
+        'first_name' => $faker->firstName,
+        'last_name' => $faker->lastName,
+        'full_name' => $faker->name,
+        'username' => $faker->userName,
+        'email' => $faker->unique()->freeEmail,
         'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'remember_token' => Str::random(10),
+        'phone' => $faker->randomElement($array = array ('010','011','012','015')) . $faker->numerify($string = '########'),
+        'password' => $faker->password,
+        'api_token' => $faker->regexify('[A-Za-z0-9]{80}'),
+        // 'image_id' => NULL,
+        'role_id' => $faker->numberBetween($min = 1, $max = 4),
+        'gender_id' => $faker->numberBetween($min = 1, $max = 3),
+        'city_id' => $faker->numberBetween($min = 1, $max = 3),
+        'address' => $faker->address,
+        'first_login' => $faker->numberBetween($min = 0, $max = 1),
+        'remember_token' => '1',
+        'created_at' => now(),
+        'updated_at' => now(),
+        'is_confirmed' => '1'
+        // randomElement($array = array ('a','b','c'))
     ];
 });
