4b1b0449728cddd25ddb8952826ba4503d72e97f
Ahmed M. Raslan <ahm3draslan@gmail.com>
2020-02-28 13:58:48 +0200

    Corrected callup_fees in doctor factory, was showing values even when offers_callup = 0

--- database/factories/DoctorFactory.php
+++ database/factories/DoctorFactory.php
@@ -8,12 +8,13 @@ use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 
 $factory->define(Doctor::class, function (Faker $faker) {
+    $callup = $faker->numberBetween($min = 0, $max = 1);
     return [
         'qualifications' => $faker->catchPhrase,
         'address' => $faker->address,
-        'offers_callup' => $faker->numberBetween($min = 0, $max = 1),
+        'offers_callup' => $callup,
         'fees' => $faker->numberBetween($min = 100, $max = 500),
-        'callup_fees' => $faker->numberBetween($min = 100, $max = 500),
+        'callup_fees' => $callup == 0 ? null : $faker->numberBetween($min = 100, $max = 500),
         'benefit' => $faker->randomFloat($nbMaxDecimals = 2, $min = 0.1, $max = 0.25),
         'cu_benefit' => $faker->numberBetween($min = 0, $max = 1),
         'created_at' => now(),
