96f6d34c55d033051939a5f2539d57ab546c78ff
Ahmed M. Raslan <ahm3draslan@gmail.com>
2020-03-02 21:12:48 +0200

    Fixed Search controller, finally =)

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -37,8 +37,8 @@ class SearchController extends Controller
             $errors []  = [ 'auth' => 'authentication failed'];
         }
         else{
-            $doctors = User::where('full_name', 'LIKE', '%'. $name .'%')->where('role_id', '3')->get();
-            if($doctors->isEmpty()){
+            $doctors_count = User::where('full_name', 'LIKE', '%'. $name .'%')->where('role_id', '3')->count();
+            if($doctors_count == 0){
                 $isFailed = true;
                 $errors[] = [
                     'error' => 'no results'
@@ -46,59 +46,73 @@ class SearchController extends Controller
             }
             else{
                 $doctors_response = [];
-                foreach ($doctors as $doctor_user){
-                    $id = $doctor_user -> id;
-                    $doc = Doctor::where('user_id', $id)->first();
-                    $spec_id = $doc -> speciality_id;
-                    $speciality = Speciality::where('id', $spec_id)->first();
-
-                    // Get the doctor's photo
-                    $image_id = $doctor_user -> image_id;
-                    $image = Image::where('id', $image_id)->first();
-                    if($image != null){
-                        $image_path = $image -> path;
-                    }
-                    else{
-                        $image_path = null;
-                    }
-
-                    // show overall rating
-                    $doc_id = $doc -> id;
-                    $appointments = Appointment::where('doctor_id', $doc_id)->get();
-                    $appointments_count = $appointments->count();
-                    $ratings = 0;
-                    $overall_rating = 0;
-                    if(!($appointments->isEmpty())){
-                        $overall_rate = 0;
-                        foreach($appointments as $appointment){
-                            $appointment_id = $appointment -> id;
-                            $rating = DoctorRating::where('appointment_id', $appointment_id)->first();
-                            $appointment_rating = 0;
-
-                            if($rating != null){
-                                $behavior = $rating -> behavior;
-                                $price = $rating -> price;
-                                $efficiency = $rating -> efficiency;
-                                $appointment_rating = ($behavior + $price + $efficiency) / 3;
-                                $overall_rate += $appointment_rating;
-                            }
+                $max = $doctors_count / 5;
+                for ($i=0; $i<= $max; $i++){
+                    $doctors = User::where('role_id', '3')
+                    ->where('full_name', 'LIKE', '%'. $name .'%')
+                    ->skip($i*5)
+                    ->take(5)
+                    ->get();
+                    foreach ($doctors as $doctor_user){
+                        $id = $doctor_user -> id;
+                        $doc = Doctor::where('user_id', $id)->first();
+                        $spec_id = $doc -> speciality_id;
+                        $speciality = Speciality::where('id', $spec_id)->first();
+                        if ($speciality == NULL){
+                            $s_name = NULL;
                         }
-                        $overall_rating = $overall_rate / $appointments_count;
-                        $ratings = $overall_rating;
-                    }
-                    else{
+                        else{
+                            $s_name = $speciality -> name;
+                        }
+                        // Get the doctor's photo
+                        $image_id = $doctor_user -> image_id;
+                        $image = Image::where('id', $image_id)->first();
+                        if($image != null){
+                            $image_path = $image -> path;
+                        }
+                        else{
+                            $image_path = null;
+                        }
+    
+                        // show overall rating
+                        $doc_id = $doc -> id;
+                        $appointments = Appointment::where('doctor_id', $doc_id)->get();
+                        $appointments_count = $appointments->count();
                         $ratings = 0;
+                        $overall_rating = 0;
+                        if(!($appointments->isEmpty())){
+                            $overall_rate = 0;
+                            foreach($appointments as $appointment){
+                                $appointment_id = $appointment -> id;
+                                $rating = DoctorRating::where('appointment_id', $appointment_id)->first();
+                                $appointment_rating = 0;
+    
+                                if($rating != null){
+                                    $behavior = $rating -> behavior;
+                                    $price = $rating -> price;
+                                    $efficiency = $rating -> efficiency;
+                                    $appointment_rating = ($behavior + $price + $efficiency) / 3;
+                                    $overall_rate += $appointment_rating;
+                                }
+                            }
+                            $overall_rating = $overall_rate / $appointments_count;
+                            $ratings = $overall_rating;
+                        }
+                        else{
+                            $ratings = 0;
+                        }
+    
+                        $doctor = [
+                            'id' => $doc -> id,
+                            'full_name' => $doctor_user -> full_name,
+                            'speciality' => $s_name,
+                            'image' => $image_path,
+                            'fees' => $doc -> fees,
+                            'overall_rating' => $ratings
+                        ];
+                        $doctors_response[] = $doctor;
                     }
-
-                    $doctor = [
-                        'id' => $doc -> id,
-                        'full_name' => $doctor_user -> full_name,
-                        'speciality' => $speciality -> name,
-                        'image' => $image_path,
-                        'fees' => $doc -> fees,
-                        'overall_rating' => $ratings
-                    ];
-                    $doctors_response[] = $doctor;
+                
                 }
                 $specialities = Speciality::all();
                 $specs = [];
