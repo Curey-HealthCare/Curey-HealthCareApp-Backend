e46bb35e3b9c1de0bd3c4094036eddd5fcc8067a
Ayloal <ayaadel3010@gmail.com>
2020-03-09 16:38:28 +0200

    create pres_day table

---
+++ database/migrations/2020_03_09_143409_pres_day.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class PresDay extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dosages', function (Blueprint $table) {
+            $table->Increments('id');
+            $table->unsignedInteger('prescription_id');
+            $table->unsignedInteger('day_id');
+            $table->timestamps();
+//            Constraints
+            $table->foreign('prescription_id')->references('id')->on('prescriptions');
+            $table->foreign('day_id')->references('id')->on('days');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pres_day');
+    }
+}
