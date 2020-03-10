60f575e26405d8b36b106fd690487e5a1698498f
Ayloal <ayaadel3010@gmail.com>
2020-03-10 20:17:19 +0200

    change in prescription controller

--- app/Http/Controllers/PrescriptionController.php
+++ app/Http/Controllers/PrescriptionController.php
@@ -85,7 +85,7 @@ class PrescriptionController extends Controller
                         'Days'=> $days,
                         'dosage_time' =>  $dosage_time
                     ];
-                    $data []=$prescription;
+                    $data []+=$prescription;
                 }
             }
         }
@@ -117,24 +117,36 @@ class PrescriptionController extends Controller
             $prescription -> start_hour = $request -> start_hour;
             $prescription -> end_date = $request -> end_date;
             $prescription -> user_id = $user -> id;
+            $prescription -> day_id = $request -> day_id;
             $prescription -> frequency= $request -> frequency;
-            //$days = new Day ; 
-            /*foreach($days as $day)
+            $days = new Day ; 
+            if($days->isEmpty())
+            {
+                $isFailed = true;
+                $errors[] = ['error' => 'no days choosen'];
+            }
+            else {
+            foreach($days as $day)
             {
               $day -> id = $request -> id ;
-
-              if( )
+              $day = Day::where('id', $prescription -> day_id)->first();
+              $dosages = new Dosage ; 
+              foreach($frequencies as $frequency)
               {
-                 
-              }
+                 $dosage -> id = $request -> id;
+                 $dosage -> dosage_time =$request -> dosage_time;
+                 $dosage -> save();
 
-            }*/
+              }
+              $day -> save();
+            }
             $prescription -> save();
 
             $data += [
                 'success' => 'prescription registered successfully'
             ];
         }
+    }
 
         $response = [
             'isFailed' => $isFailed,
