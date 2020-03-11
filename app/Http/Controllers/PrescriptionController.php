832d11f95bf3e828500338bc5e0b5a1583adf8d3
ahmedalmoselhy <ahmedalmoselhy.slm@gmail.com>
2020-03-11 13:14:24 +0200

    fix spacings

--- app/Http/Controllers/PrescriptionController.php
+++ app/Http/Controllers/PrescriptionController.php
@@ -128,16 +128,16 @@ class PrescriptionController extends Controller
             else {
             foreach($days as $day)
             {
-              $day -> id = $request -> id ;
-              $day = Day::where('id', $prescription -> day_id)->first();
-              $dosages = new Dosage ;
-              foreach($frequencies as $frequency)
-              {
-                 $dosage -> id = $request -> id;
-                 $dosage -> dosage_time =$request -> dosage_time;
-                 $dosage -> save();
-              }
-              $day -> save();
+                $day -> id = $request -> id ;
+                $day = Day::where('id', $prescription -> day_id)->first();
+                $dosages = new Dosage ;
+                foreach($frequencies as $frequency)
+                {
+                    $dosage -> id = $request -> id;
+                    $dosage -> dosage_time =$request -> dosage_time;
+                    $dosage -> save();
+                }
+                $day -> save();
             }
             $prescription -> save();
 
