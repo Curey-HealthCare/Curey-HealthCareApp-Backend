e807ae211b5810c95695d9b39bc91c898bde1907
ahmedalmoselhy <ahmedalmoselhy.slm@gmail.com>
2020-03-14 00:23:34 +0200

    create forget password table

---
+++ database/migrations/2020_03_13_221739_create_forget_password_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateForgetPasswordTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('forget_password', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('email')->nullable();
+            $table->unsignedInteger('code');
+            $table->unsignedInteger('count')->default('0');
+            $table->timestamps();
+
+            $table->foreign('email')->references('email')->on('users')
+                ->onDelete('SET NULL')->onUpdate('CASCADE');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('forget_password');
+    }
+}
