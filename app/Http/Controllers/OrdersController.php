83d0809d8194a30298b74355437eb052c6609d29
ahmedalmoselhy <ahmedalmoselhy.slm@gmail.com>
2020-03-17 20:38:57 +0200

    add seperators

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -96,55 +96,6 @@ class OrdersController extends Controller
                         ];
                         $orders_response[] = $order_response;
                     }
-                    // Build Order Response
-                    // //id of pharmacy
-                    // $pharm_id = $phar -> id;
-                    // //$pharm = Pharmacy::where('user_id',$pharm_id)->first();
-                    // //name of pharmacy
-                    // $Uid = $phar -> user_id;
-                    // $ph_name = User::find($Uid);
-                    // //pharmacy image
-                    // $image_id = $ph_name -> image_id;
-                    // $image = Image::where('id', $image_id)->first();
-                    // if($image != null){
-                    //     $image_path = $image -> path;
-                    // }
-                    // else{
-                    //     $image_path = null;
-                    // }
-                    // $ph_address = $phar -> address ;
-                    //     //response
-                    // $pharmacy = [
-                    //     'id'=> $pharm_id,
-                    //     'name'=> $ph_name,
-                    //     'image'=> $image_path,
-                    //     'address'=> $ph_address
-                    // ];
-                    // $pharmacy_response []= $pharmacy;
-                    // $orders = Order::where('pharmacy_id',$pharm_id)->get();
-                    // $order_response = [];
-                    // foreach($orders as $order)
-                    // {
-                    //     //id of the order
-                    //     $order_id = $order -> id ;
-                    //     $ord = OrderDetail::where('order_id',$order_id)->first();
-                    //     //amount
-                    //     $amount = $ord -> amount ;
-                    //     //id of product
-                    //     $pro_id = $ord -> product_id;
-                    //     $product = Product::where('id',$pro_id)->first();
-                    //     //product name
-                    //     $pro_name = $product -> name;
-                    //     $pro_price = $product -> price;
-                    //     $total_price = $amount * $pro_price;
-
-                    //     $order_response = [
-                    //         'order_id'=> $order_id,
-                    //         'product_id'=> $pro_id,
-                    //         'name'=> $pro_name,
-                    //         'total_price'=> $total_price
-                    //     ];
-                    // }
                 }
                 $data = [
                     'orders' => $orders_response
@@ -247,6 +198,17 @@ class OrdersController extends Controller
 
         return response()->json($response);
     }
+
+    /* Web Tailored Functions */
+    /* ***************************************************************************************************
+    ******************************************************************************************************
+    ******************************************************************************************************
+    ******************************************************************************************************
+    ******************************************************************************************************
+    ******************************************************************************************************
+    ******************************************************************************************************
+    ******************************************************************************************************
+    */
 
     public function webShowOrders(Request $request)
     {
