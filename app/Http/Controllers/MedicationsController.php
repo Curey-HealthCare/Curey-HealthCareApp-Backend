17ffcbba5e88167f4aa9135543093b25074efc8f
Ahmed M. Raslan <ahm3draslan@gmail.com>
2020-03-17 22:42:27 +0200

    Fixed another stupid error

--- app/Http/Controllers/MedicationsController.php
+++ app/Http/Controllers/MedicationsController.php
@@ -170,7 +170,7 @@ class MedicationsController extends Controller
 
                 // get the pharmacies that has this product and exist in my city
                 $pharmacies_product = ProductPharmacy::where('product_id', $product_id)->get();
-                if($pharmacies_product == []){
+                if($pharmacies_product != []){
                     // echo $pharmacies_product;
                     foreach($pharmacies_product as $pharmacy_product){
                         $pharmacy_id = $pharmacy_product -> pharmacy_id;
