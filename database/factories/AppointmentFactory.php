43a4a174a7287749cb2e7f61be304723987244ad
Ahmed M. Raslan <ahm3draslan@gmail.com>
2020-03-18 15:05:47 +0200

    test

--- database/factories/AppointmentFactory.php
+++ database/factories/AppointmentFactory.php
@@ -8,7 +8,7 @@ use Faker\Generator as Faker;
 $factory->define(Appointment::class, function (Faker $faker) {
     return [
         'is_callup' => $faker->numberBetween($min = 0, $max = 1),
-        'appointment_time' => $faker->dateTimeThisMonth($max = 'now', $interval = '+ 1 hours'),
+        'appointment_time' => $faker->dateTimeThisMonth($max = 'now', $startDate = '2020-03-01 00:00:00', $interval = '+ 1 hours'),
         'diagnosis' => $faker->catchPhrase,
         'duration' => $faker->numberBetween($min = 1, $max = 3),
         'last_checkup' => now(),
