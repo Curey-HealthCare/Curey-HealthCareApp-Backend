10add19d9e8767264c6199dacbd460d48d5e0293
ahmedalmoselhy <ahmedalmoselhy.slm@gmail.com>
2020-03-26 00:48:51 +0200

    clone cart functions for web api

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -197,4 +197,189 @@ class CartController extends Controller
 
         return response()->json($response);
     }
+
+    // web functions
+    public function webCreate(Request $request){
+        $isFailed = false;
+        $data = [];
+        $errors =  [];
+        $api_token = $request -> api_token;
+        $user = User::where('api_token', $api_token)->first();
+
+        if ($user == null){
+            $isFailed = true;
+            $errors += [
+                'auth' => 'authentication failed'
+            ];
+        }
+        else{
+            $product = $request -> product;
+            if($product == null){
+                $isFailed = true;
+                $errors += [
+                    'error' => 'can not register an empty product'
+                ];
+            }
+            else{
+                $item_id = $product['id'];
+                $existing = Cart::where(['user_id' => $user -> id, 'product_id' => $item_id])->get();
+                if($existing -> isEmpty()){
+                    $cart_item = new Cart;
+                    $cart_item -> user_id = $user -> id;
+                    $cart_item -> product_id = $product['id'];
+                    $cart_item -> amount = $product['amount'];
+
+                    $cart_item -> save();
+                    $data += [
+                        'success' => 'item added to cart'
+                    ];
+                }
+                else{
+                    $isFailed = true;
+                    $errors += [
+                        'error' => 'this item already exists in your cart'
+                    ];
+                }
+            }
+        }
+
+        $response = [
+            'isFailed' => $isFailed,
+            'data' => $data,
+            'errors' => $errors
+        ];
+
+        return response()->json($response);
+    }
+    public function webRead(Request $request){
+        $isFailed = false;
+        $data = [];
+        $errors =  [];
+        $api_token = $request -> api_token;
+        $user = User::where('api_token', $api_token)->first();
+
+        if ($user == null){
+            $isFailed = true;
+            $errors += [
+                'auth' => 'authentication failed'
+            ];
+        }
+        else{
+            $cart_items = Cart::where('user_id', $user -> id)->get();
+            if($cart_items -> isEmpty()){
+                $isFailed = true;
+                $errors += [
+                    'error' => 'you do not have any items'
+                ];
+            }
+            else{
+                // get details about each item in the cart
+                $products = [];
+                foreach($cart_items as $item){
+                    $product_pharmacy_id = $item -> id;
+                    $amount = $item -> amount;
+                    $product = null;
+                    // get product id and pharmacy id
+                    $product_pharmacy = ProductPharmacy::where('id', $product_pharmacy_id)->first();
+                    if($product_pharmacy == null){
+                        $isFailed = true;
+                        $errors += [
+                            'error' => 'can not retrieve item data'
+                        ];
+                    }
+                    else{
+                        $product_id = $product_pharmacy -> product_id;
+                        $pharmacy_id = $product_pharmacy -> pharmacy_id;
+                        // get all product information
+                        $product = Product::where('id', $product_id)->first();
+                        if($product != null){
+
+                        }
+                    }
+                }
+            }
+        }
+
+        $response = [
+            'isFailed' => $isFailed,
+            'data' => $data,
+            'errors' => $errors
+        ];
+
+        return response()->json($response);
+    }
+    public function webUpdate(Request $request){
+        $isFailed = false;
+        $data = [];
+        $errors =  [];
+        $api_token = $request -> api_token;
+        $user = User::where('api_token', $api_token)->first();
+
+        if ($user == null){
+            $isFailed = true;
+            $errors += [
+                'auth' => 'authentication failed'
+            ];
+        }
+        else{
+            $product = $request -> product;
+            if($product == null){
+                $isFailed = true;
+                $errors += [
+                    'error' => 'can not update an empty product'
+                ];
+            }
+            else{
+                Cart::where(['user_id' => $user -> id, 'product_id' => $product['id']])->update(['amount' => $product['amount']]);
+                $data += [
+                    'success' => 'updated successfully'
+                ];
+            }
+        }
+
+        $response = [
+            'isFailed' => $isFailed,
+            'data' => $data,
+            'errors' => $errors
+        ];
+
+        return response()->json($response);
+    }
+    public function webDelete(Request $request){
+        $isFailed = false;
+        $data = [];
+        $errors =  [];
+        $api_token = $request -> api_token;
+        $user = User::where('api_token', $api_token)->first();
+
+        if ($user == null){
+            $isFailed = true;
+            $errors += [
+                'auth' => 'authentication failed'
+            ];
+        }
+        else{
+            $product = $request -> product;
+            if($product == null){
+                $isFailed = true;
+                $errors += [
+                    'error' => 'can not update an empty product'
+                ];
+            }
+            else{
+                Cart::where(['user_id' => $user -> id, 'product_id' => $product['id']])->delete();
+                $data += [
+                    'success' => 'deleted successfully'
+                ];
+            }
+        }
+
+        $response = [
+            'isFailed' => $isFailed,
+            'data' => $data,
+            'errors' => $errors
+        ];
+
+        return response()->json($response);
+    }
 }
