04a200b139993a1ef10c7630b7295db110dc0b1e
ahmedalmoselhy <ahmedalmoselhy.slm@gmail.com>
2020-03-25 23:50:38 +0200

    create cart table migration and model

---
+++ database/migrations/2020_03_25_234410_create_cart_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCartTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cart', function (Blueprint $table) {
+            $table->Increments('id');
+            $table->unsignedInteger('user_id')->nullable(true);
+            $table->unsignedInteger('product_id')->nullable(true);
+            $table->unsignedInteger('amount')->nullable(true);
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onDelete('SET NULL')->onUpdate('CASCADE');
+            $table->foreign('product_id')->references('id')->on('products_pharmacies')
+                ->onDelete('SET NULL')->onUpdate('CASCADE');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cart');
+    }
+}
