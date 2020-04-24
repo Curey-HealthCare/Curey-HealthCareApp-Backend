b493b6c0ddb519a8631bc179abd654f5bdcbd7f4
Ahmed AlMoselhy <ahmedalmoselhy.slm@gmail.com>
2020-04-24 19:46:47 +0200

    check amount before order submit

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -154,8 +154,17 @@ class OrdersController extends Controller
                     }
                     $pharmacies = [];
                     foreach($products_pharmacies as $product_pharmacy){
+//                        check the available ammount of each product in the order
                         $pharmacy_id_request = $product_pharmacy['id'];
-                        $pharmacy_id = ProductPharmacy::select('pharmacy_id')->where('id', $pharmacy_id_request)->first()-> pharmacy_id;
+                        $the_pharmacy = ProductPharmacy::select('pharmacy_id')->where('id', $pharmacy_id_request)->first();
+                        if($the_pharmacy -> count < $product_pharmacy['amount']){
+                            $isFailed = true;
+                            $errors += [
+                                'stock' => 'the amount you ordered is not available'
+                            ];
+                            break;
+                        }
+                        $pharmacy_id = $the_pharmacy -> pharmacy_id;
                         if(in_array($pharmacy_id, $pharmacies)){
                             continue;
                         }
@@ -164,55 +173,57 @@ class OrdersController extends Controller
                         }
                     }
                     $order = null;
-                    for($i = 0; $i < count($pharmacies); $i++){
-                        $user_id = $user -> id;
-                        $products = [];
-                        $order = new Order;
-                        $order -> pharmacy_id = $pharmacy_id;
-                        $order -> user_id = $user_id;
-                        $order -> address = $order_address;
-                        $order -> delivery_type = 1;
-                        if($order -> save()){
-                            $order_tracking = new OrderTracking;
-                            $order_tracking -> user_id = $user -> id;
-                            $order_tracking -> order_id = $order -> id;
-                            $order_tracking -> save();
+                    if($isFailed == false){
+                        for($i = 0; $i < count($pharmacies); $i++){
+                            $user_id = $user -> id;
                             $products = [];
-                            foreach ($products_pharmacies as $product_pharmacy) {
-                                $pharmacy_id_request = $product_pharmacy['id'];
-                                $pharmacy_id = ProductPharmacy::select('pharmacy_id')->where('id', $pharmacy_id_request)->first()-> pharmacy_id;
-                                if($pharmacy_id == $pharmacies[$i]){
-                                    $product_id = ProductPharmacy::select('product_id')->where('id', $pharmacy_id_request)->first()-> product_id;
-                                    $amount = $product_pharmacy['amount'];
-                                    $product = [
-                                        'id' => $product_id,
-                                        'amount' => $amount,
-                                    ];
-                                    $products[] = $product;
+                            $order = new Order;
+                            $order -> pharmacy_id = $pharmacy_id;
+                            $order -> user_id = $user_id;
+                            $order -> address = $order_address;
+                            $order -> delivery_type = 1;
+                            if($order -> save()){
+                                $order_tracking = new OrderTracking;
+                                $order_tracking -> user_id = $user -> id;
+                                $order_tracking -> order_id = $order -> id;
+                                $order_tracking -> save();
+                                $products = [];
+                                foreach ($products_pharmacies as $product_pharmacy) {
+                                    $pharmacy_id_request = $product_pharmacy['id'];
+                                    $pharmacy_id = ProductPharmacy::select('pharmacy_id')->where('id', $pharmacy_id_request)->first()-> pharmacy_id;
+                                    if($pharmacy_id == $pharmacies[$i]){
+                                        $product_id = ProductPharmacy::select('product_id')->where('id', $pharmacy_id_request)->first()-> product_id;
+                                        $amount = $product_pharmacy['amount'];
+                                        $product = [
+                                            'id' => $product_id,
+                                            'amount' => $amount,
+                                        ];
+                                        $products[] = $product;
+                                    }
+                                    else{
+                                        continue;
+                                    }
                                 }
-                                else{
-                                    continue;
+                                foreach($products as $pro){
+                                    $order_detail = new OrderDetail;
+                                    $order_detail -> order_id = $order -> id;
+                                    $order_detail -> product_id = $pro['id'];
+                                    $order_detail -> amount = $pro['amount'];
+                                    $order_detail -> save();
                                 }
-                            }
-                            foreach($products as $pro){
-                                $order_detail = new OrderDetail;
-                                $order_detail -> order_id = $order -> id;
-                                $order_detail -> product_id = $pro['id'];
-                                $order_detail -> amount = $pro['amount'];
-                                $order_detail -> save();
                             }
                         }
+                        foreach($products_pharmacies as $deproduct){
+                            $product_id = $deproduct['id'];
+                            Cart::where(['user_id' => $user -> id, 'product_id' => $product_id])->delete();
+                            $existing = ProductPharmacy::find($product_id);
+                            $remaining = $existing -> count - $deproduct['amount'];
+                            ProductPharmacy::where('id', $product_id)->update(['count' => $remaining]);
+                        }
+                        $data += [
+                            'success' => 'your orders have been sent'
+                        ];
                     }
-                    foreach($products_pharmacies as $deproduct){
-                        $product_id = $deproduct['id'];
-                        Cart::where(['user_id' => $user -> id, 'product_id' => $product_id])->delete();
-                        $existing = ProductPharmacy::find($product_id);
-                        $remaining = $existing -> count - $deproduct['amount'];
-                        ProductPharmacy::where('id', $product_id)->update(['count' => $remaining]);
-                    }
-                    $data += [
-                        'success' => 'your orders have been sent'
-                    ];
                 }
             }
         }
@@ -425,8 +436,17 @@ class OrdersController extends Controller
                     }
                     $pharmacies = [];
                     foreach($products_pharmacies as $product_pharmacy){
+//                        check the available ammount of each product in the order
                         $pharmacy_id_request = $product_pharmacy['id'];
-                        $pharmacy_id = ProductPharmacy::select('pharmacy_id')->where('id', $pharmacy_id_request)->first()-> pharmacy_id;
+                        $the_pharmacy = ProductPharmacy::select('pharmacy_id')->where('id', $pharmacy_id_request)->first();
+                        if($the_pharmacy -> count < $product_pharmacy['amount']){
+                            $isFailed = true;
+                            $errors += [
+                                'stock' => 'the amount you ordered is not available'
+                            ];
+                            break;
+                        }
+                        $pharmacy_id = $the_pharmacy -> pharmacy_id;
                         if(in_array($pharmacy_id, $pharmacies)){
                             continue;
                         }
@@ -435,55 +455,57 @@ class OrdersController extends Controller
                         }
                     }
                     $order = null;
-                    for($i = 0; $i < count($pharmacies); $i++){
-                        $user_id = $user -> id;
-                        $products = [];
-                        $order = new Order;
-                        $order -> pharmacy_id = $pharmacy_id;
-                        $order -> user_id = $user_id;
-                        $order -> address = $order_address;
-                        $order -> delivery_type = 1;
-                        if($order -> save()){
-                            $order_tracking = new OrderTracking;
-                            $order_tracking -> user_id = $user -> id;
-                            $order_tracking -> order_id = $order -> id;
-                            $order_tracking -> save();
+                    if($isFailed == false){
+                        for($i = 0; $i < count($pharmacies); $i++){
+                            $user_id = $user -> id;
                             $products = [];
-                            foreach ($products_pharmacies as $product_pharmacy) {
-                                $pharmacy_id_request = $product_pharmacy['id'];
-                                $pharmacy_id = ProductPharmacy::select('pharmacy_id')->where('id', $pharmacy_id_request)->first()-> pharmacy_id;
-                                if($pharmacy_id == $pharmacies[$i]){
-                                    $product_id = ProductPharmacy::select('product_id')->where('id', $pharmacy_id_request)->first()-> product_id;
-                                    $amount = $product_pharmacy['amount'];
-                                    $product = [
-                                        'id' => $product_id,
-                                        'amount' => $amount,
-                                    ];
-                                    $products[] = $product;
+                            $order = new Order;
+                            $order -> pharmacy_id = $pharmacy_id;
+                            $order -> user_id = $user_id;
+                            $order -> address = $order_address;
+                            $order -> delivery_type = 1;
+                            if($order -> save()){
+                                $order_tracking = new OrderTracking;
+                                $order_tracking -> user_id = $user -> id;
+                                $order_tracking -> order_id = $order -> id;
+                                $order_tracking -> save();
+                                $products = [];
+                                foreach ($products_pharmacies as $product_pharmacy) {
+                                    $pharmacy_id_request = $product_pharmacy['id'];
+                                    $pharmacy_id = ProductPharmacy::select('pharmacy_id')->where('id', $pharmacy_id_request)->first()-> pharmacy_id;
+                                    if($pharmacy_id == $pharmacies[$i]){
+                                        $product_id = ProductPharmacy::select('product_id')->where('id', $pharmacy_id_request)->first()-> product_id;
+                                        $amount = $product_pharmacy['amount'];
+                                        $product = [
+                                            'id' => $product_id,
+                                            'amount' => $amount,
+                                        ];
+                                        $products[] = $product;
+                                    }
+                                    else{
+                                        continue;
+                                    }
                                 }
-                                else{
-                                    continue;
+                                foreach($products as $pro){
+                                    $order_detail = new OrderDetail;
+                                    $order_detail -> order_id = $order -> id;
+                                    $order_detail -> product_id = $pro['id'];
+                                    $order_detail -> amount = $pro['amount'];
+                                    $order_detail -> save();
                                 }
-                            }
-                            foreach($products as $pro){
-                                $order_detail = new OrderDetail;
-                                $order_detail -> order_id = $order -> id;
-                                $order_detail -> product_id = $pro['id'];
-                                $order_detail -> amount = $pro['amount'];
-                                $order_detail -> save();
                             }
                         }
+                        foreach($products_pharmacies as $deproduct){
+                            $product_id = $deproduct['id'];
+                            Cart::where(['user_id' => $user -> id, 'product_id' => $product_id])->delete();
+                            $existing = ProductPharmacy::find($product_id);
+                            $remaining = $existing -> count - $deproduct['amount'];
+                            ProductPharmacy::where('id', $product_id)->update(['count' => $remaining]);
+                        }
+                        $data += [
+                            'success' => 'your orders have been sent'
+                        ];
                     }
-                    foreach($products_pharmacies as $deproduct){
-                        $product_id = $deproduct['id'];
-                        Cart::where(['user_id' => $user -> id, 'product_id' => $product_id])->delete();
-                        $existing = ProductPharmacy::find($product_id);
-                        $remaining = $existing -> count - $deproduct['amount'];
-                        ProductPharmacy::where('id', $product_id)->update(['count' => $remaining]);
-                    }
-                    $data += [
-                        'success' => 'your orders have been sent'
-                    ];
                 }
             }
         }
