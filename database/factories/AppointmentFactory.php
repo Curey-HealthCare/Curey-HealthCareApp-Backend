5c59bace30c42e24574990ee602b34ba137a1aaf
Ahmed M. Raslan <ahm3draslan@gmail.com>
2020-04-28 00:22:55 +0200

    Removed duration from appointment factory

--- database/factories/AppointmentFactory.php
+++ database/factories/AppointmentFactory.php
@@ -9,7 +9,6 @@ $factory->define(Appointment::class, function (Faker $faker) {
     return [
         'is_callup' => $faker->numberBetween($min = 0, $max = 1),
         'diagnosis' => $faker->catchPhrase,
-        'duration' => 1,
         'last_checkup' => now(),
         're_examination' => $faker->numberBetween($min = 0, $max = 1),
         'created_at' => now(),
