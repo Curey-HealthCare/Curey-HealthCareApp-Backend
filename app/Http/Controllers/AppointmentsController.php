4749c559b2f08b17d819ed677a74c189844192d2
Ahmed M. Raslan <ahm3draslan@gmail.com>
2020-05-09 03:05:18 +0200

    Another attempt to fix error when returning values

--- app/Http/Controllers/AppointmentsController.php
+++ app/Http/Controllers/AppointmentsController.php
@@ -48,13 +48,26 @@ class AppointmentsController extends Controller
                     $doc_id = $app -> doctor_id;
                     $doc = Doctor::where('id',$doc_id)->first();
                     $add = $doc -> address;
+                    if($add == NULL){
+                        $add = 'Not set';
+                    }
                     $isCallUp = $app -> is_callup;
                     if($isCallUp == 1)
                     {
                         $fees = $doc -> callup_fees;
+                        if($fees == NULL){
+                            $fees = 0;
+                        }
                     }
                     else{
                         $fees = $doc -> fees;
+                        if($fees == NULL){
+                            $fees = 0;
+                        }
+                    }
+                    $docDur = $doc -> duration;
+                    if($docDur == NULL){
+                        $docDur = 0;
                     }
                     $reExamine = $app -> re_examination;
                     if($reExamine==1)
@@ -65,7 +78,11 @@ class AppointmentsController extends Controller
                         $checkup = null;
                     }
                     $spec_id = $doc -> speciality_id;
-                    $spec= Speciality::find($spec_id);
+                    if($spec_id == NULL){
+                        $spec = 'Not set';
+                    }else{
+                        $spec= Speciality::find($spec_id) -> name;
+                    }
                     $Uid = $doc -> user_id;
                     $user_doc = User::find($Uid);
                     $img_id = $user_doc -> image_id;
@@ -83,9 +100,9 @@ class AppointmentsController extends Controller
                         'full_name'=> $user_doc -> full_name,
                         'address' => $add,
                         'image' => $image_url,
-                        'speciality'=> $spec -> name,
+                        'speciality'=> $spec,
                         'app_time' => $app -> appointment_time,
-                        'duration' => $doc -> duration,
+                        'duration' => $docDur,
                         'fees' => $fees,
                         'last_checkup' => $checkup,
                         'is_callup' => $app -> is_callup,
@@ -432,18 +449,18 @@ class AppointmentsController extends Controller
                     {
                         $fees = $doc -> callup_fees;
                         if($fees == NULL){
-                            $fees = 'Not set';
+                            $fees = 0;
                         }
                     }
                     else{
                         $fees = $doc -> fees;
                         if($fees == NULL){
-                            $fees = 'Not set';
+                            $fees = 0;
                         }
                     }
                     $docDur = $doc -> duration;
                     if($docDur == NULL){
-                        $docDur = 'Not set';
+                        $docDur = 0;
                     }
                     $reExamine = $app -> re_examination;
                     if($reExamine==1)
